02663a63c67c020969d0137d408791e9983925e4
kumm <kumm@webstar.hu>
2018-04-15 20:28:48 +0200

    Explicitly define push transport

--- flow-cdi-itest/src/main/java/com/wcs/vaadin/flow/cdi/itest/push/WebsocketPushView.java
+++ flow-cdi-itest/src/main/java/com/wcs/vaadin/flow/cdi/itest/push/WebsocketPushView.java
@@ -3,12 +3,13 @@ package com.wcs.vaadin.flow.cdi.itest.push;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.shared.ui.Transport;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
 @Route("websocket")
-@Push
+@Push(transport = Transport.WEBSOCKET)
 public class WebsocketPushView extends Div {
     @Inject
     PushComponent pushComponent;
